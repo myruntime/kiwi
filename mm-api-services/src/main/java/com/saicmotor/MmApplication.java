3850980d61048a62e6084ac1010676541615a3c8
zhuhuliang <393884298@qq.com>
2019-08-12 23:36:18 +0800

    类重命名

---
+++ mm-api-services/src/main/java/com/saicmotor/MmApplication.java
@@ -0,0 +1,22 @@
+package com.saicmotor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hello world!
+ */
+public class MmApplication {
+
+    private static String mic;
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+
+        mic = "Hello World!";
+        System.out.println(mic);
+    }
+
+
+
+}
--- mm-services/src/main/java/com/saicmotor/TmMmPart.java
+++ mm-services/src/main/java/com/saicmotor/TmMmPart.java
@@ -4,7 +4,7 @@ package com.saicmotor;
  * Hello world!
  *
  */
-public class App 
+public class TmMmPart
 {
     public static void main( String[] args )
     {
